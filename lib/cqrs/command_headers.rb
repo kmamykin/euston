6dd77049a8749e8345105517bf10538f67a11dd7
Guy Boertje <guyboertje@gmail.com>
2011-09-06 10:55:07 +0100

    add log_completion directive to command_headers

--- lib/cqrs/command_headers.rb
+++ lib/cqrs/command_headers.rb
@@ -1,11 +1,12 @@
 module Cqrs
   class CommandHeaders
-    attr_reader :id, :type, :version
+    attr_reader :id, :type, :version, :log_completion
 
-    def initialize id, type, version
+    def initialize id, type, version, log_completion = false
       @id = id
       @type = type
       @version = version
+      @log_completion = log_completion
     end
 
     def to_hash
@@ -13,11 +14,12 @@ module Cqrs
         :id => id,
         :type => type,
         :version => version
+        :log_completion => log_completion
       }
     end
 
     def self.from_hash hash
-      self.new hash[:id], hash[:type].to_sym, hash[:version]
+      self.new hash[:id], hash[:type].to_sym, hash[:version], ( hash[:log_completion] || false )
     end
   end
 end
