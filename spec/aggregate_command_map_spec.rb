c734f295a0f47ae480b49c454111840782a04d97
Guy Boertje <guyboertje@gmail.com>
2011-09-09 18:19:45 +0100

    make aggregate root spec behave

--- spec/aggregate_command_map_spec.rb
+++ spec/aggregate_command_map_spec.rb
@@ -7,29 +7,29 @@ module Cqrs
     let(:guid1) {Cqrs.uuid.generate}
     let(:guid2) {Cqrs.uuid.generate}
 
-    let(:command_cw) { { :headers => Cqrs::CommandHeaders.new(Cqrs.uuid.generate, :create_widget, 1),
+    let(:command_cw) { { :headers => CommandHeaders.new(Cqrs.uuid.generate, :create_widget, 1),
                       :body => { :id => guid1} } }
-    let(:command_iw) { { :headers => Cqrs::CommandHeaders.new(Cqrs.uuid.generate, :import_widget, 1),
+    let(:command_iw) { { :headers => CommandHeaders.new(Cqrs.uuid.generate, :import_widget, 1),
                       :body => { :id => guid1, :imported_count => 5 } } }
-    let(:command_aw) { { :headers => Cqrs::CommandHeaders.new(Cqrs.uuid.generate, :log_access_to_widget, 1),
+    let(:command_aw) { { :headers => CommandHeaders.new(Cqrs.uuid.generate, :log_access_to_widget, 1),
                       :body => { :widget_id => guid1 } } }
-    let(:command_cp) { { :headers => Cqrs::CommandHeaders.new(Cqrs.uuid.generate, :create_product, 1),
+    let(:command_cp) { { :headers => CommandHeaders.new(Cqrs.uuid.generate, :create_product, 1),
                       :body => { :id => guid2} } }
-    let(:command_ip) { { :headers => Cqrs::CommandHeaders.new(Cqrs.uuid.generate, :import_product, 1),
+    let(:command_ip) { { :headers => CommandHeaders.new(Cqrs.uuid.generate, :import_product, 1),
                       :body => { :id => guid2, :imported_count => 5 } } }
-    let(:command_ap) { { :headers => Cqrs::CommandHeaders.new(Cqrs.uuid.generate, :log_access_to_product, 1),
+    let(:command_ap) { { :headers => CommandHeaders.new(Cqrs.uuid.generate, :log_access_to_product, 1),
                       :body => { :product_id => guid2 } } }
 
     describe "when creating new Aggregates" do
-      let(:aggregate) { Cqrs::Sample::Widget.new( Cqrs.uuid.generate ) }
-      let(:aggregate2) { Cqrs::Sample::Product.new( Cqrs.uuid.generate ) }
+      let(:aggregate) { Sample::Widget.new( Cqrs.uuid.generate ) }
+      let(:aggregate2) { Sample::Product.new( Cqrs.uuid.generate ) }
 
       it "then side effects are seen" do
 
         aggregate.committed_commands.should have(0).items
         aggregate2.committed_commands.should have(0).items
 
-        Cqrs::Sample::Widget.new( Cqrs.uuid.generate )
+        Sample::Widget.new( Cqrs.uuid.generate )
 
         AggregateCommandMap.map.should have(2).items
 
@@ -95,7 +95,6 @@ module Cqrs
         AggregateCommandMap.deliver_command command_iw[:headers], command_iw[:body]
         AggregateCommandMap.deliver_command command_aw[:headers], command_aw[:body]
         aggregate.uncommitted_events.should have(3).items
-        ap aggregate.uncommitted_events
       end
     end
   end
--- spec/aggregate_root_spec.rb
+++ spec/aggregate_root_spec.rb
@@ -1,18 +1,23 @@
-describe 'aggregate root' do
-  context 'duplicate command consumption' do
-    let(:aggregate) { Cqrs::Sample::Widget.new }
-    let(:aggregate2) { Cqrs::Sample::Widget.new }
-    let(:command) { { :headers => Cqrs::CommandHeaders.new(Cqrs.uuid.generate, :import_widget, 1),
-                      :body => { :id => Cqrs.uuid.generate } } }
 
-    it 'does not handle the same command twice' do
-      aggregate.consume_command command[:headers], command[:body]
-      aggregate.uncommitted_events.should have(1).item
+require File.expand_path("../spec_helper", __FILE__)
 
-      aggregate.uncommitted_events.each { |e| aggregate2.replay_event e.headers, e.body }
-      
-      aggregate2.consume_command command[:headers], command[:body]      
-      aggregate2.uncommitted_events.should have(0).items
+module Cqrs
+  describe 'aggregate root' do
+    context 'duplicate command consumption' do
+      let(:aggregate) { Sample::Widget.new }
+      let(:aggregate2) { Sample::Widget.new }
+      let(:command) { { :headers => CommandHeaders.new(Cqrs.uuid.generate, :create_widget, 1),
+                        :body => { :id => Cqrs.uuid.generate } } }
+
+      it 'does not handle the same command twice' do
+        aggregate.consume_command command[:headers], command[:body]
+        aggregate.uncommitted_events.should have(1).item
+
+        aggregate.uncommitted_events.each { |e| aggregate2.replay_event e.headers, e.body }
+
+        aggregate2.consume_command command[:headers], command[:body]
+        aggregate2.uncommitted_events.should have(0).items
+      end
     end
   end
 end
