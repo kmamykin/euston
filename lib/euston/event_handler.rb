87436b936b9319c67429b0971e71c4fd249b481a
Lee Henson <lee.m.henson@gmail.com>
2011-09-29 19:34:38 +0100

    rename 'consumes' in event handlers to 'subscribes'

--- lib/euston/event_handler.rb
+++ lib/euston/event_handler.rb
@@ -3,7 +3,7 @@ module Euston
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def consumes type, version, &consumer
+      def subscribes type, version, &consumer
         define_method "__event_handler__#{type}__#{version}" do |*args|
           instance_exec *args, &consumer
         end
