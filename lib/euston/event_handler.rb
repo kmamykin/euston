5715d9e92b4a0172346f7a0c701358377369ba81
Lee Henson <lee.m.henson@gmail.com>
2011-09-30 18:50:49 +0100

    Allow user to specify the aggregate root id from an event body

--- lib/euston/event_handler.rb
+++ lib/euston/event_handler.rb
@@ -3,7 +3,19 @@ module Euston
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def subscribes type, version, &consumer
+      def subscribes type, version, opts = nil, &consumer
+        if self.include? Euston::AggregateRoot
+          opts = opts || { :id => :id }
+
+          define_method "__id_from_event_#{type}__v#{version}__" do |event|
+            if opts[:id].respond_to? :call
+              opts[:id].call event
+            else
+              event[opts[:id]]
+            end
+          end
+        end
+
         define_method "__event_handler__#{type}__#{version}" do |*args|
           instance_exec *args, &consumer
         end
