e17e8905e9d01f0110d5af2081af048f70625e54
Lee Henson <lee.m.henson@gmail.com>
2012-05-11 14:02:26 +0100

    message_bus.handle_message didn't correctly handle calls when the handler class was explicitly provided

--- lib/euston/mongo/message_bus.rb
+++ lib/euston/mongo/message_bus.rb
@@ -9,24 +9,31 @@ class MessageBus
   end
 
   def handle_message message, handler = nil
-    handlers = [handler || @global_message_handler_map.find_message_handlers(message)].flatten
+    handler_types = if handler.nil?
+      @global_message_handler_map.find_message_handlers(message).map { |h| h[:handler] }
+    else
+      [ handler ]
+    end
 
-    handlers.each do |handler_description|
+    handler_types.each { |handler_type| invoke_handler handler_type, message }
+  end
+
+  private
+
+  def invoke_handler handler_type, message
+    mapping = handler_type.message_map.get_mapping_for_message message
+
+    if handler_type.included_modules.include? EventSource
       start_time = Time.now.to_f
-      handler_type = handler_description[:handler]
-      mapping = handler_type.message_map.get_mapping_for_message message
+      event_source_id = message[:body][mapping[:identifier]]
+      history = @event_store.get_history(event_source_id) || EventSourceHistory.new(id: event_source_id)
 
-      if handler_type.included_modules.include? EventSource
-        event_source_id = message[:body][mapping[:identifier]]
-        history = @event_store.get_history event_source_id
-
-        handler_type.new(@message_class_finder, history).when(:commit_created) do |commit|
-          commit.duration = Time.now.to_f - start_time
-          @event_store.put_commit commit
-        end.consume message
-      else
-        handler_type.new.consume message
-      end
+      handler_type.new(@message_class_finder, history).when(:commit_created) do |commit|
+        commit.duration = Time.now.to_f - start_time
+        @event_store.put_commit commit
+      end.consume message
+    else
+      handler_type.new.consume message
     end
   end
 end
