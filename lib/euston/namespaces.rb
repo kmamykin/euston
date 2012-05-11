7c56bd4589501993e56ea410fad8fa063dd872dd
Lee Henson <lee.m.henson@gmail.com>
2012-05-11 14:01:18 +0100

    namespaces object should dereference string versions of constants before handing out lists of message handler types

--- lib/euston/namespaces.rb
+++ lib/euston/namespaces.rb
@@ -6,6 +6,31 @@ module Euston
       @message_handlers = [opts[:message_handlers]].flatten.compact.uniq
     end
 
-    attr_reader :commands, :events, :message_handlers
+    def commands
+      dereference_string_pointers @commands
+    end
+
+    def events
+      dereference_string_pointers @events
+    end
+
+    def message_handlers
+      dereference_string_pointers @message_handlers
+    end
+
+    private
+
+    def dereference_string_pointers collection
+      string_pointers = collection.find_all { |namespace| namespace.is_a? String }.to_a
+
+      string_pointers.each do |namespace|
+        ConstantLoader.new.when(
+          hit:  ->(ns) { collection.delete(namespace); collection << ns },
+          miss: ->{ raise "Failed to locate namespace: #{namespace}" }
+        ).load namespace
+      end
+
+      collection
+    end
   end
 end
