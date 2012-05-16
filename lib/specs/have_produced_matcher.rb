0558a688bd1e3d0aa7be437b4c2eca956e1a72ec
Lee Henson <lee.m.henson@gmail.com>
2012-05-16 18:50:53 +0100

    only eval the code in the have_produced_matcher if RSpec is loaded

--- lib/specs/have_produced_matcher.rb
+++ lib/specs/have_produced_matcher.rb
@@ -1,29 +1,31 @@
-RSpec::Matchers.define :have_produced do |number|
-  match do |actual|
-    get_value(actual) == number
-  end
+if Object.const_defined? 'RSpec'
+  RSpec::Matchers.define :have_produced do |number|
+    match do |actual|
+      get_value(actual) == number
+    end
 
-  chain :command  do
-    @attribute = :commands
-  end
+    chain :command  do
+      @attribute = :commands
+    end
 
-  chain :commands do
-    @attribute = :commands
-  end
+    chain :commands do
+      @attribute = :commands
+    end
 
-  chain :event do
-    @attribute = :events
-  end
+    chain :event do
+      @attribute = :events
+    end
 
-  chain :events do
-    @attribute = :events
-  end
+    chain :events do
+      @attribute = :events
+    end
 
-  failure_message_for_should do |actual|
-    "expected event source to have produced #{number} #{@attribute} but it actually produced #{get_value actual}"
-  end
+    failure_message_for_should do |actual|
+      "expected event source to have produced #{number} #{@attribute} but it actually produced #{get_value actual}"
+    end
 
-  def get_value actual
-    actual.send(@attribute).size
+    def get_value actual
+      actual.send(@attribute).size
+    end
   end
 end
