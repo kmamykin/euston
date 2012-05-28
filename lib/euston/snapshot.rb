3424f98ac4e0e915028da67566c74b8e64539675
Lee Henson <lee.m.henson@gmail.com>
2012-05-28 17:23:46 +0100

    add duration and timestamp attributes to snapshots

--- lib/euston/snapshot.rb
+++ lib/euston/snapshot.rb
@@ -3,16 +3,19 @@ module Euston
     def initialize options
       options = {
         body: {},
-        version: 1
+        version: 1,
+        timestamp: Time.now.utc
       }.merge options
 
       @event_source_id          = options[:event_source_id]
       @sequence                 = options[:sequence]
       @type                     = options[:type]
       @version                  = options[:version]
+      @timestamp                = options[:timestamp]
       @body                     = options[:body]
     end
 
-    attr_reader :event_source_id, :sequence, :type, :version, :body
+    attr_reader :event_source_id, :sequence, :type, :version, :timestamp, :body
+    attr_accessor :duration
   end
 end
