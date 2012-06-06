f5e99a85b925a25df4e6842664594cbaf732503f
Lee Henson <lee.m.henson@gmail.com>
2012-06-06 19:39:52 +0100

    add level? methods to null logger

--- lib/euston/null_logger.rb
+++ lib/euston/null_logger.rb
@@ -10,5 +10,14 @@ module Euston
     def warn(progname = nil, &block);  end
     def error(progname = nil, &block); end
     def fatal(progname = nil, &block); end
+
+    def debug?
+      false
+    end
+
+    alias_method :info?, :debug?
+    alias_method :warn?, :debug?
+    alias_method :error?, :debug?
+    alias_method :fatal?, :debug?
   end
 end
